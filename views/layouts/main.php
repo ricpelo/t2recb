696e1cd7781f72ff078db903d814d298221105f6
Ricardo Pérez <ricpelo@gmail.com>
2021-05-04 21:16:55 +0200

    Limpia el menú principal

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -43,9 +43,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Inicio', 'url' => ['zapatos/index']],
             ['label' => Usuarios::cantidadEnCarrito(), 'url' => ['carritos/ver'], 'encode' => false],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
